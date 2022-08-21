f02ba4de93c4e05415c235633cb9cd586cdc046d
Aleksandr Gavrilov <alex.gavrilov999@gmail.com>
2022-08-21 12:00:48 +0300

    IG-6 - Unnecessary indents removed

--- invaitGenerator.py
+++ invaitGenerator.py
@@ -35,90 +35,90 @@ else:
             print()
             shehe = input('Кому адресовано приглашение (ей/ему)?\n')
 #настройки
+
+if shehe == 'ей':
+    print()
+    name = input('Её имя? (в им. п.)\n')
+elif shehe == 'ему':
+    print()
+    name = input('Его имя? (в им. п.)\n')
+else:
+    s = 'ей'
+    ss = 'ему'
+    while shehe not in [s, ss]:
+        print( Back.RED )
+        print()
+        print('Выбранно неправильное действие,\nвыберите (ей/ему).')
+        print( Back.GREEN )
+        shehe = input('Кому адресовано приглашение (ей/ему)?\n')
+        if shehe == s or shehe == ss:
+            print()
+            name = input('Введите имя.\n')
 if decision == 'начать':
-    if shehe == 'ей':
+    print()
+    appeal = input('Как мне обращаться? (на ты/на вы).\n')
+    a = 'на ты'
+    aa = 'на вы'
+    while appeal not in [a, aa]:
+        print(Back.RED)
         print()
-        name = input('Её имя? (в им. п.)\n')
-    elif shehe == 'ему':
-        print()
-        name = input('Его имя? (в им. п.)\n')
-    else:
-        s = 'ей'
-        ss = 'ему'
-        while shehe not in [s, ss]:
-            print( Back.RED )
-            print()
-            print('Выбранно неправильное действие,\nвыберите (ей/ему).')
-            print( Back.GREEN )
-            shehe = input('Кому адресовано приглашение (ей/ему)?\n')
-            if shehe == s or shehe == ss:
-                print()
-                name = input('Введите имя.\n')
-    if decision == 'начать':
-        print()
+        print('Выбранно неправильное действие,\nвыберите (на ты/на вы).')
+        print(Back.GREEN)
         appeal = input('Как мне обращаться? (на ты/на вы).\n')
-        a = 'на ты'
-        aa = 'на вы'
-        while appeal not in [a, aa]:
-            print(Back.RED)
+        if appeal == a or appeal == aa:
             print()
-            print('Выбранно неправильное действие,\nвыберите (на ты/на вы).')
-            print(Back.GREEN)
-            appeal = input('Как мне обращаться? (на ты/на вы).\n')
-            if appeal == a or appeal == aa:
-                print()
-    if decision == 'начать':
-        print()
-        measure = input('Какое мероприятие?\n')
-    if decision == 'начать':
-        print()
-        place = input('Адрес встречи?\n')
-    if decision == 'начать':
-        print()
-        time = input('Время встречи?\n')
-    if decision == 'начать':
-        print( Back.YELLOW )
-        print()
-        print('Отлично, последний шаг')
-        nomber = input('Контактный номер?\n')
-
-    if appeal == 'на вы':
-        age = 'запомните'
-    if appeal == 'на ты':
-        age = 'запомнишь'
-    if appeal == 'на вы':
-        age2 = 'вас'
-    if appeal == 'на ты':
-    	age2 = 'тебя'
-    if appeal == 'на вы':
-        age3 = 'вы'
-    if appeal == 'на ты':
-        age3 = 'ты'
-    if shehe == 'ей':
-        pol = 'Дорогая'
-    if shehe == 'ему':
-    	pol = 'Дорогой'
-
-    print( Back.MAGENTA )
-#письмо
-    print('           Приглашение')
-    print(pol + ' ' + name + ', приглашаю ' + age2 + ' на')
-    print(measure + '! Этот день ' + age3 + ' точно')
-    print( age + ' на долго, в хорошем смысле;)')
-    print('будет много развлекательных программ и')
-    print('прочего, от ' + age2 + ' требуется только')
-    print('хорошего настроения. С нетерпением жду')
-    print(age2 + '.')
+if decision == 'начать':
     print()
-    print('Место встречи ' + place)
-    print('Время встречи ' + time)
-    print('Номер связи ' + nomber)
+    measure = input('Какое мероприятие?\n')
+if decision == 'начать':
     print()
+    place = input('Адрес встречи?\n')
+if decision == 'начать':
+    print()
+    time = input('Время встречи?\n')
+if decision == 'начать':
     print( Back.YELLOW )
     print()
-    print('Спасибо за пользование! В дальнейшем мы будем выпускать\nобновления, в которых будем добавлять новые\nфункции и исправление ошибок в сиситеие;)')
-    print(Back.GREEN)
-    exitt = input('Для выхода нажмите Enter.\n')
+    print('Отлично, последний шаг')
+    nomber = input('Контактный номер?\n')
+
+if appeal == 'на вы':
+    age = 'запомните'
+if appeal == 'на ты':
+    age = 'запомнишь'
+if appeal == 'на вы':
+    age2 = 'вас'
+if appeal == 'на ты':
+    age2 = 'тебя'
+if appeal == 'на вы':
+    age3 = 'вы'
+if appeal == 'на ты':
+    age3 = 'ты'
+if shehe == 'ей':
+    pol = 'Дорогая'
+if shehe == 'ему':
+    pol = 'Дорогой'
+
+print( Back.MAGENTA )
+#письмо
+print('           Приглашение')
+print(pol + ' ' + name + ', приглашаю ' + age2 + ' на')
+print(measure + '! Этот день ' + age3 + ' точно')
+print( age + ' на долго, в хорошем смысле;)')
+print('будет много развлекательных программ и')
+print('прочего, от ' + age2 + ' требуется только')
+print('хорошего настроения. С нетерпением жду')
+print(age2 + '.')
+print()
+print('Место встречи ' + place)
+print('Время встречи ' + time)
+print('Номер связи ' + nomber)
+print()
+print( Back.YELLOW )
+print()
+print('Спасибо за пользование! В дальнейшем мы будем выпускать\nобновления, в которых будем добавлять новые\nфункции и исправление ошибок в сиситеие;)')
+print(Back.GREEN)
+exitt = input('Для выхода нажмите Enter.\n')
 
 
 
